cea3a7f5f5bdca0d587e990e491dc848bb695510
SpoonOil <nieves.jon.8@gmail.com>
2023-08-18 07:18:44 -0400

    add link to redlaser new eco sim, deprecated eco sim frontend

--- ecoSim/ecoScript.js
+++ ecoSim/ecoScript.js
@@ -1,8 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".toolList");
-
+const closeButton = document.querySelector(".warning-close-button")
+const banner = document.querySelector(".warning-banner-container")
+closeButton.addEventListener("click", closeBanner);
 hamburger.addEventListener("click", mobileMenu);
 
+function closeBanner() {
+    banner.classList.add("hidden")
+}
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
