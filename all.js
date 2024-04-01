a043bf86123a9e9c3679444cab0b3e59bd4fae94
emilplane <129400320+emilplane@users.noreply.github.com>
2024-04-01 15:49:56 +0000

    nothing going on here

---
+++ all.js
@@ -0,0 +1,24 @@
+console.log(document.querySelector("body"))
+document.querySelector("body").insertAdjacentHTML("beforeend", `
+    <style>
+        body {
+            transform: rotate(180deg);
+        }
+    </style>
+`)
+setTimeout(() => {
+    document.querySelector("body").insertAdjacentHTML("beforeend", `
+        <style>
+            body {
+                transition: 1s
+            }
+        </style>
+    `)
+    document.querySelector("body").insertAdjacentHTML("beforeend", `
+        <style>
+            body {
+                transform: rotate(0deg);
+            }
+        </style>
+    `)
+}, 2000);
