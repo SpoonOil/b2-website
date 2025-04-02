19ef762c0d09dbfdf6e083c816385cc19110c2ad
emilplane <emilplane@b2popology.com>
2025-04-01 23:27:53 -0500

    April fools style is automated and supports accessibility settings

--- all.js
+++ all.js
@@ -1,24 +1,34 @@
-console.log(document.querySelector("body"))
-document.querySelector("body").insertAdjacentHTML("beforeend", `
-    <style>
-        body {
-            transform: rotate(180deg);
-        }
-    </style>
-`)
-setTimeout(() => {
+// Don't play the effect if the user has selected the prefer-reduced-motion setting on their device
+if (isAprilFools() && !checkReducedMotion()) {
     document.querySelector("body").insertAdjacentHTML("beforeend", `
         <style>
             body {
-                transition: 1s
+                transform: rotate(180deg);
             }
         </style>
     `)
-    document.querySelector("body").insertAdjacentHTML("beforeend", `
-        <style>
-            body {
-                transform: rotate(0deg);
-            }
-        </style>
-    `)
-}, 2000);
+    setTimeout(() => {
+        document.querySelector("body").insertAdjacentHTML("beforeend", `
+            <style>
+                body {
+                    transition: 1s
+                }
+            </style>
+        `)
+        document.querySelector("body").insertAdjacentHTML("beforeend", `
+            <style>
+                body {
+                    transform: rotate(0deg);
+                }
+            </style>
+        `)
+    }, 1000);
+}
+
+function isAprilFools() {
+    return new Date().getMonth() === 3 && new Date().getDate() === 1
+}
+
+function checkReducedMotion() {
+    return window.matchMedia('(prefers-reduced-motion: reduce)').matches
+}
